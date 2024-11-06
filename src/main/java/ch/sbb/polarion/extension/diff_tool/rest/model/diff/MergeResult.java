b504869fe3410eb47489062c3f06eef92550050f
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-11-06 14:34:23 +0100

    feat: merge report
    Refs: #133

--- src/main/java/ch/sbb/polarion/extension/diff_tool/rest/model/diff/MergeResult.java
+++ src/main/java/ch/sbb/polarion/extension/diff_tool/rest/model/diff/MergeResult.java
@@ -1,13 +1,11 @@
 package ch.sbb.polarion.extension.diff_tool.rest.model.diff;
 
+import ch.sbb.polarion.extension.diff_tool.report.MergeReport;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Builder
@@ -25,31 +23,6 @@ public class MergeResult {
     @Schema(description = "Indicates whether the merge was not authorized")
     private boolean mergeNotAuthorized;
 
-    @Builder.Default
-    @Schema(description = "List of WorkItem pairs that were created during the merge operation", implementation = WorkItemsPair.class)
-    private List<WorkItemsPair> createdPairs = new ArrayList<>();
-
-    @Builder.Default
-    @Schema(description = "List of WorkItem pairs that were modified during the merge operation", implementation = WorkItemsPair.class)
-    private List<WorkItemsPair> modifiedPairs = new ArrayList<>();
-
-    @Builder.Default
-    @Schema(description = "List of WorkItem pairs that caused conflicts during the merge operation", implementation = WorkItemsPair.class)
-    private List<WorkItemsPair> conflictedPairs = new ArrayList<>();
-
-    @Builder.Default
-    @Schema(description = "List of WorkItem pairs which logically are prohibited to be merged", implementation = WorkItemsPair.class)
-    private List<WorkItemsPair> prohibitedPairs = new ArrayList<>();
-
-    @Builder.Default
-    @Schema(description = "List of WorkItems from source document in one project which do not have counterparts in another project of target document", implementation = WorkItemsPair.class)
-    private List<WorkItemsPair> notPaired = new ArrayList<>();
-
-    @Builder.Default
-    @Schema(description = "List of WorkItem pairs that were moved during the merge operation", implementation = WorkItemsPair.class)
-    private List<WorkItemsPair> movedPairs = new ArrayList<>();
-
-    @Builder.Default
-    @Schema(description = "List of WorkItem pairs that were not moved during the merge operation", implementation = WorkItemsPair.class)
-    private List<WorkItemsPair> notMovedPairs = new ArrayList<>();
+    @Schema(description = "Merge report with detailed information about the merge operation", implementation = MergeReport.class)
+    private MergeReport mergeReport;
 }
