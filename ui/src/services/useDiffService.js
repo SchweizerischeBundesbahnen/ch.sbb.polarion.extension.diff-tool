40364218fd5426fbcccacb2ba8cea868d52ebd1f
Dzmitry Yurtsevich <dzmitry.yurtsevich@sbb.ch>
2024-11-07 18:51:39 +0100

    fix: Fix false positive of skipping mirrored and child items (quick, not full fix) (#149)
    * fix: Fix false positive of skipping mirroredchild items (quick, not full fix)

--- ui/src/services/useDiffService.js
+++ ui/src/services/useDiffService.js
@@ -113,7 +113,8 @@ export default function useDiffService() {
       // Exclude mirrored and child items in case of moving items (such redundant selection is done automatically on UI for better usability)
       if (pair.leftWorkItem && pair.rightWorkItem && (pair.leftWorkItem.movedOutlineNumber || pair.rightWorkItem.outlineNumber)) {
         for (let filtered of filteredPairs) {
-          if (filtered.leftWorkItem && pair.leftWorkItem.outlineNumber.startsWith(filtered.leftWorkItem.outlineNumber)) {
+          if (filtered.leftWorkItem && (pair.leftWorkItem.outlineNumber.startsWith(filtered.leftWorkItem.outlineNumber + ".")
+              || pair.leftWorkItem.outlineNumber.startsWith(filtered.leftWorkItem.outlineNumber + "-"))) {
             redundant = true;
             break;
           }
