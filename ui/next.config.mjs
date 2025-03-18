ba803a58b3667d0af694328b14179623d3e5d169
e550443 <dzmitry.yurtsevich@sbb.ch>
2025-03-18 11:34:50 +0100

    chore: JS testing, add Cypress code coverage

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -16,13 +16,46 @@ export default (phase, { defaultConfig }) => {
           source: '/polarion/wiki/:path*',
           destination: `${process.env.NEXT_PUBLIC_BASE_URL}/polarion/wiki/:path*`,
         },
-      ]
+      ],
+
     }
   }
 
-  return {
+  const nextConfig = {
     output: 'export',
     basePath: '/polarion/diff-tool-app/ui/app',
     distDir: '../src/main/resources/webapp/diff-tool-app/app'
+  };
+
+  if (process.env.CYPRESS === 'true' && process.env.NEXT_PUBLIC_COVERAGE === 'true') {
+    nextConfig.webpack = (config, { dev, isServer }) => {
+      // Only add instrumentation in non-production environments
+      if (!isServer) {
+        console.log('Enabling code coverage for Cypress...');
+        config.module.rules.push({
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: [
+            /node_modules/,
+            /\.next\//,
+            /cypress\//,
+            /.spec./,
+            /.test./,
+          ],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['next/babel'],
+              plugins: ['istanbul']
+            }
+          }
+        });
+      }
+
+      // If you have existing webpack config, merge it here
+
+      return config;
+    };
   }
+
+  return nextConfig;
 }
