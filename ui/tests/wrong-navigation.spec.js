6925695a1f067f852a00f7c61a087b7a3151d5c2
e550443 <dzmitry.yurtsevich@sbb.ch>
2025-04-08 14:22:21 +0200

    chore: try to fix GitHub build

--- ui/tests/wrong-navigation.spec.js
+++ ui/tests/wrong-navigation.spec.js
@@ -4,21 +4,21 @@ import { test, expect } from '@playwright/test';
 test.describe("wrong navigation", () => {
 
   test('handles root route', async ({ page }) => {
-    await page.goto('/');
+    await page.goto('http://127.0.0.1:3000/');
 
     await expect(page.locator("h1")).toHaveText("404");
     await expect(page.locator("h2")).toHaveText("This page could not be found.");
   });
 
   test('handles wrong route', async ({ page }) => {
-    await page.goto('/fake');
+    await page.goto('http://127.0.0.1:3000/fake');
 
     await expect(page.locator("h1")).toHaveText("404");
     await expect(page.locator("h2")).toHaveText("This page could not be found.");
   });
 
   test('handles missing params of documents diffing', async ({ page }) => {
-    await page.goto('/documents');
+    await page.goto('http://127.0.0.1:3000/documents');
 
     await expect(page.locator("h2")).toHaveText("Error occurred loading diff data!");
     await expect(page.locator("p").filter({ hasText: "Following parameters are missing:" }))
@@ -26,7 +26,7 @@ test.describe("wrong navigation", () => {
   });
 
   test('handles missing params of workitems diffing', async ({ page }) => {
-    await page.goto('/workitems');
+    await page.goto('http://127.0.0.1:3000/workitems');
 
     await expect(page.locator("h2")).toHaveText("Error occurred loading diff data!");
     await expect(page.locator("p").filter({ hasText: "Following parameters are missing:" }))
@@ -34,7 +34,7 @@ test.describe("wrong navigation", () => {
   });
 
   test('handles missing params of collections diffing', async ({ page }) => {
-    await page.goto('/collections');
+    await page.goto('http://127.0.0.1:3000/collections');
 
     await expect(page.locator("h2")).toHaveText("Error occurred loading diff data!");
     await expect(page.locator("p").filter({ hasText: "Following parameters are missing:" }))
