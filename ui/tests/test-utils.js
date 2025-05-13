89f56faa4791c6f01a356898f8d3b9f77d679e68
Alex Nikonov <alexander.nikonoff@sbb.ch>
2025-05-13 15:53:04 +0200

    chore: js tests for content of documents diff & merge (#247)

--- ui/tests/test-utils.js
+++ ui/tests/test-utils.js
@@ -9,53 +9,17 @@ const fixtureDataProvider = (fixtureFile) => {
 };
 
 const workItemsDiffFixtureDataProvider = (requestBody) => {
-  let fixtureFile;
-  if (!requestBody.leftWorkItem) {
-    if (requestBody.rightWorkItem.id === "DP-11570") {
-      fixtureFile = "NONE_DP-11570.json";
-    }
-  } else if (!requestBody.rightWorkItem) {
-    if (requestBody.leftWorkItem.id === "EL-156") {
-      fixtureFile = "EL-156_NONE.json";
-    } else if (requestBody.leftWorkItem.id === "EL-11600") {
-      fixtureFile = "EL-11600_NONE.json";
-    }
-  } else if (requestBody.leftWorkItem.id === "EL-183" && requestBody.rightWorkItem.id === "DP-11545") {
-    fixtureFile = "EL-183_DP-11545.json";
-  } else if (requestBody.leftWorkItem.id === "EL-184" && requestBody.rightWorkItem.id === "DP-11546") {
-    fixtureFile = "EL-184_DP-11546.json";
-  } else if (requestBody.leftWorkItem.id === "EL-144" && requestBody.rightWorkItem.id === "DP-11549") {
-    fixtureFile = "EL-144_DP-11549.json";
-  } else if (requestBody.leftWorkItem.id === "EL-145" && requestBody.rightWorkItem.id === "DP-11550") {
-    fixtureFile = "EL-145_DP-11550.json";
-  } else if (requestBody.leftWorkItem.id === "EL-101" && requestBody.rightWorkItem.id === "DP-11551") {
-    fixtureFile = "EL-101_DP-11551.json";
-  } else if (requestBody.leftWorkItem.id === "EL-146" && requestBody.rightWorkItem.id === "DP-11552") {
-    fixtureFile = "EL-146_DP-11552.json";
-  } else if (requestBody.leftWorkItem.id === "EL-4977" && requestBody.rightWorkItem.id === "DP-11559") {
-    fixtureFile = "EL-4977_DP-11559.json";
-  } else if (requestBody.leftWorkItem.id === "EL-11575" && requestBody.rightWorkItem.id === "EL-11575") {
-    fixtureFile = "EL-11575_EL-11575.json";
-  } else if (requestBody.leftWorkItem.id === "EL-153" && requestBody.rightWorkItem.id === "DP-11560") {
-    fixtureFile = "EL-153_DP-11560.json";
-  } else if (requestBody.leftWorkItem.id === "EL-11572" && requestBody.rightWorkItem.id === "DP-11571") {
-    fixtureFile = "EL-11572_DP-11571.json";
-  } else if (requestBody.leftWorkItem.id === "EL-157" && requestBody.rightWorkItem.id === "DP-11562") {
-    fixtureFile = "EL-157_DP-11562.json";
-  } else if (requestBody.leftWorkItem.id === "EL-158" && requestBody.rightWorkItem.id === "DP-11563") {
-    fixtureFile = "EL-158_DP-11563.json";
-  } else if (requestBody.leftWorkItem.id === "EL-159" && requestBody.rightWorkItem.id === "DP-11564") {
-    fixtureFile = "EL-159_DP-11564.json";
-  } else if (requestBody.leftWorkItem.id === "EL-156" && requestBody.rightWorkItem.id === "DP-11599") {
-    fixtureFile = "EL-156_DP-11599.json";
-  }
 
-  if (fixtureFile) {
-    const filePath = path.join(__dirname, 'fixtures', fixtureFile);
+  const left = requestBody.leftWorkItem ? requestBody.leftWorkItem.id : 'NONE';
+  const right = requestBody.rightWorkItem ? requestBody.rightWorkItem.id : 'NONE';
+  const fixtureFile = `${left}_${right}.json`;
+
+  const filePath = path.join(__dirname, 'fixtures', fixtureFile);
+  if (fs.existsSync(filePath)) {
     const fixtureData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     return JSON.stringify(fixtureData)
   } else {
-    console.log("diff fixture not found: ", requestBody);
+    console.log(`fixture file not found: ${fixtureFile}`);
     return {}
   }
 };
@@ -102,6 +66,26 @@ const fieldsMergeFixtureDataProvider = (requestBody) => {
     fixtureFile = "docRevision-field-merge.json";
   } else if (requestBody.fieldIds.includes("docLanguage")) {
     fixtureFile = "docLanguage-field-merge.json";
+  }
+
+  if (fixtureFile) {
+    const filePath = path.join(__dirname, 'fixtures', fixtureFile);
+    const fixtureData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    return JSON.stringify(fixtureData)
+  } else {
+    console.log("field merge fixture not found: ", requestBody);
+    return {}
+  }
+};
+
+const contentMergeFixtureDataProvider = (requestBody) => {
+  let fixtureFile;
+  if (requestBody.pairs[0].leftWorkItemId === "EL-63012") {
+    fixtureFile = "content-merge.json";
+  } else if (requestBody.pairs[0].leftWorkItemId === "EL-63013") {
+    fixtureFile = "content-merge-not-authorized.json";
+  } else if (requestBody.pairs[0].leftWorkItemId === "EL-63016") {
+    fixtureFile = "content-merge-aborted.json";
   }
 
   if (fixtureFile) {
@@ -186,6 +170,25 @@ export const test = base.extend({
             status: 200,
             contentType: 'application/json',
             body: fieldsMergeFixtureDataProvider(requestBody)
+          });
+        });
+      },
+
+      // Mock "/diff/documents-content" endpoint which emulates merge operation and returns JSON response depending on data in request body
+      mockContentMergeEndpoint: async () => {
+        await page.route("**/merge/documents-content", route => {
+          const request = route.request();
+          let requestBody;
+          try {
+            requestBody = request.postDataJSON();
+          } catch (e) {
+            requestBody = {};
+          }
+
+          route.fulfill({
+            status: 200,
+            contentType: 'application/json',
+            body: contentMergeFixtureDataProvider(requestBody)
           });
         });
       }
