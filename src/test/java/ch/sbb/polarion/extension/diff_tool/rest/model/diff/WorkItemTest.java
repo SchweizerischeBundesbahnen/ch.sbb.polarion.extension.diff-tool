0a5523652326af74f3179fd3aff14a21d870a2e1
e550443 <dzmitry.yurtsevich@sbb.ch>
2026-01-20 10:37:52 +0100

    chore: add unit tests

--- src/test/java/ch/sbb/polarion/extension/diff_tool/rest/model/diff/WorkItemTest.java
+++ src/test/java/ch/sbb/polarion/extension/diff_tool/rest/model/diff/WorkItemTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Objects;
 
